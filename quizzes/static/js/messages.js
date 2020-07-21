2753a7da590d162a908a3ef5ab1b606462750c58
1-0 <1_0@usa.com>
2020-07-21 12:53:53 +0300

    Use toast for messages

--- quizzes/static/js/messages.js
+++ quizzes/static/js/messages.js
@@ -3,4 +3,5 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Modal.init(elems, 1);
     var instance = M.Modal.getInstance(elems[0]);
     instance.open();
+//    M.toast({html: 'Data saved'})
 });

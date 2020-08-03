01bc775b02323e3d8a45a1e4626e34ce7e77f86d
1-0 <1_0@usa.com>
2020-08-03 20:32:35 +0300

    Add show_user get request

--- quizzes/usercards/forms.py
+++ quizzes/usercards/forms.py
@@ -28,11 +28,8 @@ class UserAll(forms.ModelForm):
 
 class UserCardAll(forms.ModelForm):
 
-    # photo = forms.ImageField()
-
     class Meta:
         model = UserCard
-        # fields = ('about', 'birthday')
         fields = ('about', 'birthday', 'photo')
         labels = {
             'about': 'About',
@@ -45,23 +42,8 @@ class UserCardAll(forms.ModelForm):
         }
 
 
-class UserInfo(forms.ModelForm):
-    """Form for the User model"""
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name')
-
-
-class UserCard(forms.ModelForm):
-    """Form for the UserCard model"""
-    class Meta:
-        model = UserCard
-        fields = ('about', 'birthday')
-        # fields = ('about', 'birthday', 'photo')
-
-
 class UserForm(forms.Form):
-    """UserForm = Form for the user"""
+    """UserForm - Form for the user"""
     first_name = forms.CharField(max_length=200, widget=forms.TextInput)
     last_name = forms.CharField(max_length=200, widget=forms.TextInput)
     about = forms.CharField(widget=forms.Textarea)
--- quizzes/usercards/views.py
+++ quizzes/usercards/views.py
@@ -6,96 +6,54 @@ from django.contrib.auth.decorators import login_required
 from .forms import *
 # from .forms import UserInfo, UserCard, UserForm
 from .models import UserCard as UCard
-import datetime
-
-
-def prepare_form_user_info(user_data):
-    UserDataFormSet = formset_factory(UserInfo, extra=2)
-    formset = UserDataFormSet(initial=[
-        {
-            'first_name': user_data.first_name,
-            'last_name': user_data.last_name,
-        }
-    ])
-    return formset[0]
-
-
-def prepare_form_usercard(user_data):
-    UserCardFormSet = formset_factory(UserCard, extra=2)
-    formset = UserCardFormSet(initial=[
-        {
-            'about': user_data.about,
-            'birthday': user_data.birthday,
-        }
-    ])
-    return formset[0]
-
-
-def prepare_form_user(user_contribute, user_data):
-    UserDataFormSet1 = formset_factory(UserForm)
-    forms = UserDataFormSet1(initial=[
-        {
-            'first_name': user_contribute.first_name,
-            'last_name': user_contribute.last_name,
-            'about': user_data.about,
-            'birthday': user_data.birthday,
-        }
-    ])
-    return forms[0]
 
 
 @login_required
 def show_user(request, user_name):
     """show_user - show user page"""
-    form = None
-    valid_user = user_name
+    forms = []
+    valid_user = False
     try:
-        user_contribute = User.objects.get(username=user_name)
+        user = User.objects.get(username=user_name)
     except:
-        user_contribute = User()
+        user = User()
     try:
-        user_data = UCard.objects.get(person=user_contribute)
+        user_data = UCard.objects.get(person_id=user.id)
     except:
         user_data = UCard()
     if request.method == 'GET':
+        forms.append(UserAll(initial={
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+            'date_joined': user.date_joined,
+        }))
+        forms.append(UserCardAll(initial={
+            'about': user_data.about,
+            'birthday': user_data.birthday,
+            'photo': user_data.photo,
+        }))
         if user_name == request.user.get_username():
-            form = prepare_form_user(user_contribute, user_data)
+            valid_user = user_name
         else:
-            valid_user = False
-    if request.method == 'POST':
-        form = UserForm(request.POST, request.FILES)
-        if form.is_valid():
-            user_contribute.first_name = form.data['form-0-first_name']
-            user_contribute.last_name = form.data['form-0-last_name']
-            user_contribute.save()
-            user_data.about = form.data['form-0-about']
-            day = form.data['form-0-birthday'].split('-')
-            user_data.birthday = datetime.date(int(day[0]), int(day[1]), int(day[2]))
-            # user_data.birthday = form.data['form-0-birthday']
-            user_data.save()
-            user_name='valid '+form.data['form-0-first_name']+form.data['form-0-last_name']+form.data['form-0-about']+form.data['form-0-birthday']
-            form = prepare_form_user(user_contribute, user_data)
-            # form.save
-        else:
-            user_name = 'not valid '+form.data['form-0-first_name'] + form.data['form-0-last_name'] + form.data['form-0-about'] + \
-                        form.data['form-0-birthday']
-
+            for i in forms.fields:
+                for f in i:
+                    i.fields[f].disabled = True
     return render(
         request,
         'usercards/show_user.html',
         {
-            'user_contribute': user_contribute,
             'user_data': user_data,
             'user_name': user_name,
             'valid_user': valid_user,
-            'form': form,
+            'forms': forms,
         }
     )
 
 
 @login_required
 def show_user_all(request, user_name):
-    """show_user_data - show user data page"""
+    """show_user_all - show user data page"""
     valid_user = False
     try:
         user_data = User.objects.get(username=user_name)
@@ -138,7 +96,7 @@ def show_user_all(request, user_name):
 
 @login_required
 def show_user_card_all(request, user_name):
-    """show_user_data - show user data page"""
+    """show_user_card_all - show user card data page"""
     form = None
     valid_user = False
     try:
@@ -153,6 +111,7 @@ def show_user_card_all(request, user_name):
         form = UserCardAll(initial={
             'about': user_data.about,
             'birthday': user_data.birthday,
+            'photo': user_data.photo,
         })
         if user_name == request.user.get_username():
             valid_user = user_name

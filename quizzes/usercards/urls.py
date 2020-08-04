b848aaaa864278935e61cb5735fa3b40f1c0a5e7
1-0 <1_0@usa.com>
2020-08-04 15:46:39 +0300

    Move all user info to show user

--- quizzes/usercards/urls.py
+++ quizzes/usercards/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('show_user/<str:user_name>', views.show_user, name='show_user'),
-    path('show_user_all/<str:user_name>', views.show_user_all, name='show_user_all'),
-    path('show_user_card_all/<str:user_name>', views.show_user_card_all, name='show_user_card_all'),
 ]
--- quizzes/usercards/views.py
+++ quizzes/usercards/views.py
@@ -21,24 +21,43 @@ def show_user(request, user_name):
         user_data = UCard.objects.get(person_id=user.id)
     except:
         user_data = UCard()
+    forms.append(UserAll(initial={
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'email': user.email,
+        'date_joined': user.date_joined,
+    }))
+    forms.append(UserCardAll(initial={
+        'about': user_data.about,
+        'birthday': user_data.birthday,
+        'photo': user_data.photo,
+    }))
     if request.method == 'GET':
-        forms.append(UserAll(initial={
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'email': user.email,
-            'date_joined': user.date_joined,
-        }))
-        forms.append(UserCardAll(initial={
-            'about': user_data.about,
-            'birthday': user_data.birthday,
-            'photo': user_data.photo,
-        }))
         if user_name == request.user.get_username():
             valid_user = user_name
         else:
             for i in forms:
                 for f in i.fields:
                     i.fields[f].disabled = True
+    elif request.method == 'POST':
+        if user_name == request.user.get_username():
+            valid_user = user_name
+            form0 = UserAll(request.POST or None, request.FILES or None, instance=user)
+            form1 = UserCardAll(request.POST or None, request.FILES or None, instance=user_data)
+            saved_form = []
+            if form0.is_valid():
+                if form0.save():
+                    saved_form.append(0)
+                    forms[0] = form0
+            if form1.is_valid():
+                if form1.save():
+                    saved_form.append(1)
+                    forms[1] = form1
+            if saved_form:
+                messages.add_message(request, messages.SUCCESS, 'Data is saved')
+            else:
+                messages.add_message(request, messages.SUCCESS, 'Data is not saved')
+
     return render(
         request,
         'usercards/show_user.html',
@@ -47,96 +66,6 @@ def show_user(request, user_name):
             'user_name': user_name,
             'valid_user': valid_user,
             'forms': forms,
-        }
-    )
-
-
-@login_required
-def show_user_all(request, user_name):
-    """show_user_all - show user data page"""
-    valid_user = False
-    try:
-        user_data = User.objects.get(username=user_name)
-    except:
-        user_data = UserAll()
-    if request.method == 'GET':
-        form = UserAll(initial={
-            'first_name': user_data.first_name,
-            'last_name': user_data.last_name,
-            'email': user_data.email,
-            'date_joined': user_data.date_joined,
-        })
-        if user_name == request.user.get_username():
-            valid_user = user_name
-        else:
-            for f in form.fields:
-                form.fields[f].disabled = True
-    elif request.method == 'POST':
-        if user_name == request.user.get_username():
-            valid_user = user_name
-            form = UserAll(request.POST or None, request.FILES or None, instance=user_data)
-            if form.is_valid():
-                if form.save():
-                    messages.add_message(request, messages.SUCCESS, 'Data saved')
-                else:
-                    messages.add_message(request, messages.SUCCESS, 'Valid Data not saved')
-            else:
-                messages.add_message(request, messages.SUCCESS, 'Not Valid Data not saved')
-    return render(
-        request,
-        'usercards/show_user_all.html',
-        {
-            'user_data': user_data,
-            'user_name': user_name,
-            'valid_user': valid_user,
-            'form': form,
-        }
-    )
-
-
-@login_required
-def show_user_card_all(request, user_name):
-    """show_user_card_all - show user card data page"""
-    form = None
-    valid_user = False
-    try:
-        user = User.objects.get(username=user_name)
-    except:
-        user = User()
-    try:
-        user_data = UCard.objects.get(person_id=user.id)
-    except:
-        user_data = UCard()
-    if request.method == 'GET':
-        form = UserCardAll(initial={
-            'about': user_data.about,
-            'birthday': user_data.birthday,
-            'photo': user_data.photo,
-        })
-        if user_name == request.user.get_username():
-            valid_user = user_name
-        else:
-            for f in form.fields:
-                form.fields[f].disabled = True
-    elif request.method == 'POST':
-        if user_name == request.user.get_username():
-            valid_user = user_name
-            form = UserCardAll(request.POST or None, request.FILES or None, instance=user_data)
-            if form.is_valid():
-                if form.save():
-                    messages.add_message(request, messages.SUCCESS, 'Data saved')
-                else:
-                    messages.add_message(request, messages.SUCCESS, 'Valid Data not saved')
-            else:
-                messages.add_message(request, messages.SUCCESS, 'Not Valid Data not saved')
-    return render(
-        request,
-        'usercards/show_user_card_all.html',
-        {
-            'user_data': user_data,
-            'user_name': user_name,
-            'valid_user': valid_user,
-            'form': form,
         }
     )
 

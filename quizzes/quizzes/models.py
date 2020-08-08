04c41a860f30ef9cffd1545a0c3a1b63a3f6815a
1-0 <1_0@usa.com>
2020-08-08 21:10:38 +0300

    Add quizzes models classes

--- quizzes/quizzes/models.py
+++ quizzes/quizzes/models.py
@@ -1,1 +1,45 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Quizzes(models.Model):
+    """Quizzes - class for quizzes content"""
+    person = models.ManyToManyField(User)
+    image = models.ImageField(
+        upload_to=settings.MEDIA_URL+'quizzes/%d-%m-%YT%H.%M.%S.%f/',
+        null=True,
+        blank=True
+    )
+    content = models.TextField()
+
+
+class Question(models.Model):
+    """Question - class for quizzes questions"""
+    quizzes = models.ForeignKey(Quizzes, on_delete=models.CASCADE)
+    image = models.ImageField(
+        upload_to=settings.MEDIA_URL+'questions/%d-%m-%YT%H.%M.%S.%f/',
+        null=True,
+        blank=True
+    )
+    content = models.TextField()
+
+
+class Answer(models.Model):
+    """Answer - class for quizzes questions answers"""
+    question = models.ManyToManyField(Question, on_delete=models.CASCADE)
+    image = models.ImageField(
+        upload_to=settings.MEDIA_URL+'answers/%d-%m-%YT%H.%M.%S.%f/',
+        null=True,
+        blank=True
+    )
+    content = models.TextField()
+    correct = models.BooleanField()
+
+
+class Comment(models.Model):
+    """Comment - class for quizzes Comments"""
+    quizzes = models.ForeignKey(Quizzes, on_delete=models.CASCADE)
+    person = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    content = models.TextField()
+    allowed = models.BooleanField(default=True)
